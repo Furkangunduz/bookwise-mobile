fd54b36f0815ad2b4bb11776150b928b430f3e8c
Furkan Gündüz <furkan.gundz00@gmail.com>
2024-12-13 23:49:54 +0300

    feat: creted first screen LES GO

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,8 @@
 import '~/global.css';
 
+import { Inter_400Regular, Inter_700Bold, useFonts } from '@expo-google-fonts/inter';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import { DarkTheme, DefaultTheme, Theme, ThemeProvider } from '@react-navigation/native';
+import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { PortalHost } from '@rn-primitives/portal';
 import { SplashScreen, Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
@@ -11,14 +12,8 @@ import { setAndroidNavigationBar } from '~/lib/android-navigation-bar';
 import { NAV_THEME } from '~/lib/constants';
 import { useColorScheme } from '~/lib/useColorScheme';
 
-const LIGHT_THEME: Theme = {
-  ...DefaultTheme,
-  colors: NAV_THEME.light,
-};
-const DARK_THEME: Theme = {
-  ...DarkTheme,
-  colors: NAV_THEME.dark,
-};
+const LIGHT_THEME = { ...DefaultTheme, colors: NAV_THEME.light };
+const DARK_THEME = { ...DarkTheme, colors: NAV_THEME.dark };
 
 export { ErrorBoundary } from 'expo-router';
 
@@ -28,33 +23,27 @@ export default function RootLayout() {
   const { colorScheme, setColorScheme, isDarkColorScheme } = useColorScheme();
   const [isColorSchemeLoaded, setIsColorSchemeLoaded] = React.useState(false);
 
+  const [fontsLoaded] = useFonts({
+    InterRegular: Inter_400Regular,
+    InterBold: Inter_700Bold,
+  });
+
   React.useEffect(() => {
     (async () => {
       const theme = await AsyncStorage.getItem('theme');
       if (Platform.OS === 'web') {
         document.documentElement.classList.add('bg-background');
       }
-      if (!theme) {
-        AsyncStorage.setItem('theme', 'dark');
-        setColorScheme('dark');
-        setIsColorSchemeLoaded(true);
-        return;
-      }
-      const colorTheme = theme === 'dark' ? 'dark' : 'light';
+      const colorTheme = theme === 'dark' ? 'dark' : theme === 'light' ? 'light' : 'dark';
       if (colorTheme !== colorScheme) {
         setColorScheme(colorTheme);
-        setAndroidNavigationBar(colorTheme);
-        setIsColorSchemeLoaded(true);
-        return;
       }
       setAndroidNavigationBar(colorTheme);
       setIsColorSchemeLoaded(true);
-    })().finally(() => {
-      SplashScreen.hideAsync();
-    });
+    })().finally(() => SplashScreen.hideAsync());
   }, []);
 
-  if (!isColorSchemeLoaded) {
+  if (!isColorSchemeLoaded || !fontsLoaded) {
     return null;
   }
 
